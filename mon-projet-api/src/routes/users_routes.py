ac18f0dc39c8f70e8b0d31abc6f92098971e906a
xavier deguercy <xavier.deguercy@gmail.com>
2026-01-09 16:15:57 +0100

    mexo 4 users_routes.py: fix function parameters order

--- mon-projet-api/src/routes/users_routes.py
+++ mon-projet-api/src/routes/users_routes.py
@@ -3,27 +3,27 @@ from fastapi import APIRouter
 
 users_routeur = APIRouter()
 
-@users_routeur.get("/users")                                       # endpoint pour récupérer tous les utilisateurs
+@users_routeur.get("/users")                             # endpoint pour récupérer tous les utilisateurs
 def get_all_users() -> list[User]:                       # fonction pour récupérer tous les utilisateurs 
    return {"resultat": []}                               # retourne une liste vide pour l'instant
 
 # query parameter ?active=False
-@users_routeur.get("/users/{user_id}")                             # endpoint pour récupérer un utilisateur par son ID
+@users_routeur.get("/users/{user_id}")                   # endpoint pour récupérer un utilisateur par son ID
 def get_user_by_id(
-    user_id: int, 
-    active: bool):
+    user_id: int,                                        # id de l'utilisateur
+    active: bool):                                       # statut actif ou inactif de l'utilisateur (query parameter)
    return {
       "id_mon_user": user_id,                            # id de l'utilisateur
       "active": active                                   # statut actif ou inactif, querry permet de faire des filtres
    }
 
-@users_routeur.post("/users/")                                     # endpoint pour créer un utilisateur
+@users_routeur.post("/users/")                           # endpoint pour créer un utilisateur
 def create_user(new_user: UserPost):                     # réceptionne un UserPost en entrée
    return {
       "Résultat": new_user
    }                                                     # retourne le nouvel utilisateur créé
 
-### ---- pathuser
+### ---- patch_user
 
 @users_routeur.patch("/users/{user_id}")
 def patch_user_by_id(
